fb861b7cde228b3e91140c33fc879e903e64b9f2
Work desktop <the.red.lemon@yandex.ru>
2023-02-21 14:08:28 +0300

    feat: add components Header and Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { Container } from "./components/Container/Container.jsx"
+import { Header } from "./components/Header/Header.jsx"
 
 export const App = () => {
-  return <h1>Привет мир!</h1>
+  return (
+    <>
+      <Header/>
+      <main>
+        <nav>
+          <Container className="navigation__container"/>
+        </nav>
+        <section></section>
+      </main>
+      <footer></footer>
+    </>
+  )
 }
 
 export default App
